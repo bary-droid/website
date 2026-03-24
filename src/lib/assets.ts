69a24797ab9dcd37c9b99578b4b98126615a56f5
Bar Yizhak Chen <bary@worldcomfinance.com>
2026-03-24 13:58:22 +0200

    feat: download and organize all Figma assets

---
+++ src/lib/assets.ts
@@ -0,0 +1,62 @@
+// Hero
+export const heroBackground = "/images/hero-background.png";
+export const heroPhoneMockup = "/images/hero-phone-mockup.png";
+
+// Partner logos (Section 1)
+export const partner1 = "/images/partner-1.png";
+export const partner2 = "/images/partner-2.png";
+export const partner3 = "/images/partner-3.png";
+export const partner4 = "/images/partner-4.png";
+export const partner5 = "/images/partner-5.png";
+export const partner6 = "/images/partner-6.png";
+
+// Money Transfer cards (Section 2)
+export const transferCard1 = "/images/transfer-card-1.png";
+export const transferCard2 = "/images/transfer-card-2.png";
+export const transferCard3 = "/images/transfer-card-3.png";
+export const transferCard4 = "/images/transfer-card-4.png";
+
+// Business Solutions (Section 3)
+export const businessIllustration = "/images/business-illustration.png";
+export const clientLogo1 = "/images/client-logo-1.png";
+export const clientLogo2 = "/images/client-logo-2.png";
+export const clientHolon = "/images/client-holon.png";
+export const clientLogo3 = "/images/client-logo-3.png";
+export const clientLogo4 = "/images/client-logo-4.png";
+export const clientLogo5 = "/images/client-logo-5.png";
+export const clientLogo6 = "/images/client-logo-6.png";
+export const clientLogo7 = "/images/client-logo-7.png";
+export const clientLogo8 = "/images/client-logo-8.png";
+
+// New Application (Section 4)
+export const appPhoneMockup = "/images/app-phone-mockup.png";
+export const glassLightBlob = "/images/glass-light-blob.png";
+
+// Feature Cards
+export const featureDigitalWallet = "/images/feature-digital-wallet.png";
+export const featureSimTopup = "/images/feature-sim-topup.png";
+
+// WPay Card (Section 5)
+export const wpayCards = "/images/wpay-cards.png";
+export const logoMax = "/images/logo-max.png";
+export const logoBankJerusalem = "/images/logo-bank-jerusalem.png";
+
+// Layout
+export const logoWorldcom = "/images/logo-worldcom.png";
+export const logoWorldcomFooter = "/images/logo-worldcom-footer.png";
+
+// Icons
+export const iconMenu = "/images/icon-menu.svg";
+export const iconLock = "/images/icon-lock.svg";
+export const iconArrowDown = "/images/icon-arrow-down.svg";
+export const iconMail = "/images/icon-mail.svg";
+export const iconPhone = "/images/icon-phone.svg";
+
+// Social
+export const socialFacebook = "/images/social-facebook.png";
+export const socialInstagram = "/images/social-instagram.png";
+export const socialLinkedin = "/images/social-linkedin.png";
+
+// App Store badges
+export const badgeGooglePlay = "/images/badge-google-play.png";
+export const badgeAppStore = "/images/badge-app-store.png";
